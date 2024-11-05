86263c1092c3c582659f80301be3fc79b6e000ef
ayaNabil <alharazinaya@gmail.com>
2024-11-05 10:46:36 +0200

    zabat akheeeeeeern

--- Assets/zzzzzzzzzzzzzzzzzzzzzz.cs
+++ Assets/zzzzzzzzzzzzzzzzzzzzzz.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class zzzzzzzzzzzzzzzzzzzzzz : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+     void Start()
     {
-        
-    }
+      while (true)
+        {
+            int nums = Random.Range(1, 21);
+            if (nums == 5) 
+                continue;
+            else if (nums == 15) 
+                break;
+            Debug.Log(nums);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
+
+
+          string[] arr = {"Cat", "Dog", "Car", "Pizza", "Hat", "Fish", "Tree", "Monkey", "Ball", "Bird"};
+        int no = 7;
+        string result = "";
+
+        while (no != 0)
+        {
+            int random = Random.Range(0, 10); // Generate a random number between 0 and 9
+            result += arr[random]+" ";
+            no--;
+        }
+
+        Debug.Log(result);
+         }
+
 }
